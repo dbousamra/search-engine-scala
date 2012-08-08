45e1de929e2c4f46cdf59fa6e5bef3aca3164e63
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2012-08-08 20:31:38 +1000

    Added InvertedIndex implementation. Much, MUCH cleaner then the java if/else clusterf**k

--- src/main/scala/search/documents/Document.scala
+++ src/main/scala/search/documents/Document.scala
@@ -2,10 +2,10 @@ package search.documents
 
 import java.io.File
 
-class Document(id: Int, file: File, words: List[String]) {
+class Document(val id: Int, val file: File, val words: List[String]) {
 
   def getWordCount(word: String) = {
     words.count(x => x.equals(word))
   }
-  
+
 }
--- src/main/scala/search/indexing/InvertedIndex.scala
+++ src/main/scala/search/indexing/InvertedIndex.scala
@@ -1,5 +1,28 @@
 package search.indexing
 
+import scala.collection.mutable.LinkedHashMap
+import search.documents.Document
+
 class InvertedIndex {
+
+  private var index = new LinkedHashMap[String, LinkedHashMap[Int, Int]]
+  private var _totalDocumentsIndexed = 0
+
+  def addDocumentToIndex(document: Document) = {
+    document.words.foreach { word =>
+      index.get(word) match {
+        case Some(x) => index.get(word).get.put(document.id, x.get(document.id).getOrElse(1))
+        case None => {
+          index.put(word, new LinkedHashMap[Int, Int])
+          index.get(word).get.put(document.id, 1)
+        }
+      }
+    }
+    incrementTotalDocumentsIndexed()
+  }
+
+  def incrementTotalDocumentsIndexed() = _totalDocumentsIndexed += 1
+
+  def totalDocumentsIndexed = _totalDocumentsIndexed
 
 }
