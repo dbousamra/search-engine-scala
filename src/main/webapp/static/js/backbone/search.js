a6907c7d166e829c90117132c364a8e5529aba19
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2012-09-25 18:38:51 +1000

    Added loading bar to backbone fetch callback

--- src/main/webapp/static/js/backbone/search.js
+++ src/main/webapp/static/js/backbone/search.js
@@ -11,16 +11,6 @@ var ResultView = Backbone.View.extend({
   }
  });
 
-// var ResultView = Backbone.View.extend({
-//   tagName : "li",
-//   className : "tweet",
-//   render : function() {
-//     // just render the tweet text as the content of this element.
-//     $(this.el).html(this.model.get("name"));
-//     return this;
-//   }
-// });
-
 var Results = Backbone.Collection.extend({
   model : Result,
   initialize : function(models, options) {
@@ -76,30 +66,14 @@ var CreateSearchView = Backbone.View.extend({
     var resultsView = new ResultsView({
       collection: results
     });
-
+    $("#result_content").empty()
     var targetSpinner = document.getElementById('loading');
-    var opts = {
-      lines: 13, // The number of lines to draw
-      length: 7, // The length of each line
-      width: 4, // The line thickness
-      radius: 10, // The radius of the inner circle
-      corners: 1, // Corner roundness (0..1)
-      rotate: 0, // The rotation offset
-      color: '#000', // #rgb or #rrggbb
-      speed: 1, // Rounds per second
-      trail: 60, // Afterglow percentage
-      shadow: false, // Whether to render a shadow
-      hwaccel: false, // Whether to use hardware acceleration
-      className: 'spinner', // The CSS class to assign to the spinner
-      zIndex: 2e9, // The z-index (defaults to 2000000000)
-      top: 'auto', // Top position relative to parent in px
-      left: 'auto' // Left position relative to parent in px
-    };
-    var spinner = new Spinner(opts).spin(targetSpinner);
+    
+    var spinner = new Spinner({ top: '10px' }).spin(targetSpinner);
     // on a successful fetch, update the collection.
     results.fetch({
       success: function(collection) {
-        $('#example_content').html(resultsView.render().el);
+        $('#result_content').html(resultsView.render().el);
         spinner.stop();
       }
     })
