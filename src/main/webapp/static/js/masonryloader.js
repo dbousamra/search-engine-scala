bd4591c7454d89144f90481f23e449c8c8e11a4b
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2012-09-27 14:32:42 +1000

    Initial redo

--- src/main/webapp/static/js/masonryloader.js
+++ src/main/webapp/static/js/masonryloader.js
@@ -2,6 +2,7 @@ var $container = $('#result_content');
 $container.imagesLoaded( function(){
 	$container.masonry({
 	  itemSelector : '.box',
+	  columnWidth: 1,
 	  // isAnimated : true,
 	  isResizable : true
 	});
