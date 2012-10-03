b3854e9b86165962c4d1ad13e9f4984dad1e6de7
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2012-10-03 16:47:48 +1000

    Added jquery filter clause. Can now select divs based on the year id

--- src/main/webapp/static/js/masonryloader.js
+++ src/main/webapp/static/js/masonryloader.js
@@ -1,3 +1,9 @@
+jQuery.expr[':'].between = function(a, b, c) { 
+   var args = c[3].split(',');
+   var val = parseInt(jQuery(a).attr("id"));
+   return val >= parseInt(args[0]) && val <= parseInt(args[1]);
+};
+
 var $container = $('#result_content');
 $container.imagesLoaded( function(){
 	$container.isotope({
